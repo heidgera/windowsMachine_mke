bdd2961fcbb5cb541c985c5e8ada562f02851591
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2017-01-05 18:06:43 -0600

    changes to wifi config

--- wifi.js
+++ wifi.js
@@ -3,9 +3,9 @@ exports.writeConfig = function(ssid, pass) {
 
   fs.writeFileSync('/etc/wpa_supplicant/wpa_supplicant.conf', '');
 
-  fs.appendFileSync('./assets/compile/build/build.cpp', 'network={' + '\n');
-  fs.appendFileSync('./assets/compile/build/build.cpp', '\tssid="' + ssid + '"\n');
-  fs.appendFileSync('./assets/compile/build/build.cpp', '\tpsk="' + pass + '"\n}');
+  fs.appendFileSync('/etc/wpa_supplicant/wpa_supplicant.conf', 'network={' + '\n');
+  fs.appendFileSync('/etc/wpa_supplicant/wpa_supplicant.conf', '\tssid="' + ssid + '"\n');
+  fs.appendFileSync('/etc/wpa_supplicant/wpa_supplicant.conf', '\tpsk="' + pass + '"\n}');
   /*fs.readFileSync(/etc/wpa_supplicant/wpa_supplicant.conf).toString().split('\n').forEach(function(line) {
     //console.log(line);
     if (line.toString().indexOf('${SETUP}') >= 0) {
